dabb1fdbcf3a3bf0dfa299ee64ca5ff757816e2b
audryanhn <audryanhn1331@gmail.com>
2025-12-02 17:33:03 +0700

    making response for jwt expired

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -17,13 +17,17 @@ export default (req: IReqUser, res: Response, next: NextFunction) => {
     response.unauthorized(res, "Unauthorized, token is invalid");
   }
 
-  const result = getUserData(token);
+  try {
+    const result = getUserData(token);
+    if (!result) {
+      response.unauthorized(res, "Middleware - Data not found");
+      return;
+    }
 
-  if (!result) {
-    response.forbidden(res, "Middleware - Data not found");
-    return;
+    (req as IReqUser).user = result;
+    next();
+  } catch (error) {
+    const err = error as unknown as Error;
+    response.unauthorized(res, `${err.message}`);
   }
-
-  (req as IReqUser).user = result;
-  next();
 };

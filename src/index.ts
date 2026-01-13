779080da1ae973d2c1e41ea0091ccbb814291313
audryanhn <audryanhn1331@gmail.com>
2026-01-13 23:12:42 +0700

    changing connect to database method to exported function

--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,62 @@
 //---------------------------- CARA KEDUA KEDUA CONNECT DB-----------------------------
-// import bodyParser from "body-parser";
-// import express from "express";
-// import router from "./routes/api";
-// import db from "./utils/database";
-
-// async function init() {
-//   try {
-//     const result = await db();
-//     console.log("db connection result : ", result);
-
-//     const app = express();
-//     const PORT = 3000;
-
-//     app.use(bodyParser.json());
-
-//     app.use("/api", router);
-
-//     app.listen(PORT, () => {
-//       console.log(`Server is running on http://localhost:${PORT}`);
-//     });
-//   } catch (error) {
-//     console.log("error", error);
-//   }
-// }
-
-// init();
-
-// --------------------- CARA PERTAMA CONNECT DB ----------------------------
-
 import bodyParser from "body-parser";
 import cors from "cors";
 import express from "express";
 import docs from "./docs/docRoutes";
 import router from "./routes/api";
-import "./utils/database";
+import db from "./utils/database";
 
-const app = express();
-const PORT = 3000;
+async function init() {
+  try {
+    const result = await db();
+    console.log("db connection result : ", result);
 
-app.use(bodyParser.json());
-app.use(cors());
+    const app = express();
+    const PORT = 3000;
 
-app.get("/", (req, res, next) => {
-  res.status(200).json({ message: "Server Is Running" });
-  next();
-});
+    app.use(bodyParser.json());
+    app.use(cors());
 
-app.use("/api", router);
-docs(app);
+    app.get("/", (req, res, next) => {
+      res.status(200).json({ status: "200 - server running properly" });
+    });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+    app.use("/api", router);
+    docs(app);
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.log("error", error);
+  }
+}
+
+init();
+
+// --------------------- CARA PERTAMA CONNECT DB ----------------------------
+
+// import bodyParser from "body-parser";
+// import cors from "cors";
+// import express from "express";
+// import docs from "./docs/docRoutes";
+// import router from "./routes/api";
+// import "./utils/database";
+
+// const app = express();
+// const PORT = 3000;
+
+// app.use(bodyParser.json());
+// app.use(cors());
+
+// app.get("/", (req, res, next) => {
+//   res.status(200).json({ message: "Server Is Running" });
+//   next();
+// });
+
+// app.use("/api", router);
+// docs(app);
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
